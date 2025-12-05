4a576736ed67171952dd4d2062ee20581a9a383d
Leandro Silva Soares <leandrogamedesigner@gmail.com>
2025-12-04 21:23:27 -0300

    ajustes de meus-dados

--- resources/views/user/meus-dados.blade.php
+++ resources/views/user/meus-dados.blade.php
@@ -6,7 +6,8 @@
             <div class="mb-6 bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
                 <div class="flex justify-between items-center">
                     <h2 class="text-xl font-semibold text-gray-800">Meus Dados</h2>
-                    <a href="{{ route('user.medium-history') }}" class="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700">
+                    <a href="{{ route('user.medium-history') }}"
+                        class="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700">
                         Ver Histórico Completo
                     </a>
                 </div>
@@ -31,14 +32,16 @@
             <livewire:entity-consecration-form :user="auth()->user()" />
             <br>
             <livewire:amaci-form :user="auth()->user()" />
-            <br>
-            <livewire:religious-course-form :user="auth()->user()" />
-            <br>
-            <livewire:initiated-mystery-form :user="auth()->user()" />
-            <br>
-            <livewire:initiated-orisha-form :user="auth()->user()" />
-            <br>
-            <livewire:divine-magic-form :user="auth()->user()" />
+            <!-- 
+                <br>
+                <livewire:religious-course-form :user="auth()->user()" />
+                <br>
+                <livewire:initiated-mystery-form :user="auth()->user()" />
+                <br>
+                <livewire:initiated-orisha-form :user="auth()->user()" />
+                <br>
+                <livewire:divine-magic-form :user="auth()->user()" />
+            -->
             <br>
             <livewire:last-temple-form :user="auth()->user()" />
         </div>
